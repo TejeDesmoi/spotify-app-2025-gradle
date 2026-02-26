868cbe7b99f5dd28162a9d757d003a7825aadfcb
Alberto Tejero Mateo <tejeromateo2002@gmail.com>
2026-02-26 17:15:37 +0100

    Created SongMemLocalDataSource

--- src/main/java/edu/iesam/features/songs/data/SongMemLocalDataSource.java
+++ src/main/java/edu/iesam/features/songs/data/SongMemLocalDataSource.java
@@ -6,22 +6,22 @@ import edu.iesam.features.songs.domain.Models.Song;
 import java.util.ArrayList;
 
 public class SongMemLocalDataSource {
-    private ArrayList<Song> authorsBdStorage = new ArrayList<>();
+    private ArrayList<Song> songArrayList = new ArrayList<>();
 
     public SongMemLocalDataSource() {
         initData();
     }
 
     private void initData(){
-        //Song song1 = new Song("1","Dont want to stop","","Ozzy Osborne","");
+        Song song1 = new Song("1","Dont want to stop","Solo","Ozzy Osborne","Metal","3:20");
         //authorsBdStorage.add(song1);
 
-        //Song song2 = new Song("2","Oasis","Inglesa","01-01-1980");
+        Song song2 = new Song("2","Marea","Patera","Marea","Rock","3:20");
         //authorsBdStorage.add(song2);
     }
 
     public ArrayList<Song> findAll(){
-        return authorsBdStorage;
+        return songArrayList;
     }
 
 }
